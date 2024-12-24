392c6fa8bd60e04bdf33c79bbd17c4232971ec6b
vishnu767592 <lekkalavishnu7675@gmail.com>
2024-12-24 07:55:30 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,10 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
